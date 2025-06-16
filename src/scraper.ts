cb71cbf31aa79f0d2ad9420fe0502b33f9514732
Mkratenstein <mattkratenstein@gmail.com>
2025-06-16 18:23:52 -0400

    perf: optimize puppeteer for resource-constrained environments

--- src/scraper.ts
+++ src/scraper.ts
@@ -18,7 +18,16 @@ export class Scraper {
         console.log('Scraper: Initializing browser.');
         this.browser = await puppeteer.launch({
             headless: true,
-            args: ['--no-sandbox', '--disable-setuid-sandbox'],
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                '--disable-dev-shm-usage',
+                '--disable-accelerated-2d-canvas',
+                '--no-first-run',
+                '--no-zygote',
+                '--single-process', // This is not recommended for all use cases, but can help in resource-constrained environments.
+                '--disable-gpu'
+            ],
         });
     }
 
